1320fe58af686b672472677341823120208612ad
Noburou TANIGUCHI <dev@nota.m001.jp>
2015-10-20 22:19:45 +0900

    Add start.sh
    Ref:
    * http://guides.fatfreecrm.com/Setup-Heroku.html
    * https://github.com/fatfreecrm/fat_free_crm/blob/0424d3ee1d9073f322d60a4b97d71dfc703a16a4/Procfile

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+bundle exec rake db:migrate
+bundle exec rake ffcrm:demo:load
+bundle exec rake ffcrm:setup:admin USERNAME=admin PASSWORD=admin EMAIL=admin@example.com
+
+bundle exec unicorn -p $PORT -c ./config/unicorn.rb
