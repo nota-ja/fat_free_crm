93a227f017df71b57d768fa38b541ba1561cd136
Noburou TANIGUCHI <dev@nota.m001.jp>
2015-10-21 19:01:05 +0900

    Don't use demo

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 set -x
 
 bundle exec rake db:migrate
-bundle exec rake ffcrm:demo:load
 bundle exec rake ffcrm:setup:admin USERNAME=admin PASSWORD=admin EMAIL=admin@example.com
 
 bundle exec unicorn -p $PORT -c ./config/unicorn.rb
